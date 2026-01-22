a53e33d8e5cfeee3f72c3b4a6959b98ba6e7f52d
Ih0bby <zzerxahoe@gmail.com>
2026-01-21 21:28:22 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -192,16 +192,16 @@ local Library = {
 
     OriginalMinSize = Vector2.new(480, 360),
     MinSize = Vector2.new(480, 360),
-    DPIScale = 1,
-    CornerRadius = 4,
+    DPIScale = 0.7,
+    CornerRadius = 10,
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
+        BackgroundColor = "1d1b26",
+        MainColor = "232330",
+        AccentColor = "426e87",
+        OutlineColor = "27232f",
+        FontColor = "ffffff",
         Font = Font.fromEnum(Enum.Font.Code),
 
         RedColor = Color3.fromRGB(255, 50, 50),
